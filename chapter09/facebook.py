813e15c912c556b794a4d6775480a22c9e9d6610
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2015-06-13 21:40:17 +0800

    added facebook selenium example

--- chapter09/facebook.py
+++ chapter09/facebook.py
@@ -1,20 +1,30 @@
 # -*- coding: utf-8 -*-
 
 import sys
-import json
-import pprint
-from downloader import Downloader
+from selenium import webdriver
 
 
-def graph(page_id):
-    D = Downloader()
-    html = D('http://graph.facebook.com/' + page_id)
-    return json.loads(html)
-
-
+def search(username, password, keyword):
+    driver = webdriver.Firefox()
+    driver.get('https://www.facebook.com')
+    driver.find_element_by_id('email').send_keys(username)
+    driver.find_element_by_id('pass').send_keys(password)
+    driver.find_element_by_id('login_form').submit()
+    driver.implicitly_wait(30)
+    # search box that is available when logged in
+    search = driver.find_element_by_id('q')
+    search.send_keys(keyword)
+    search.submit()
+    # Add code to scrape data of interest from Facebook search here
+    #driver.close()
+ 
+    
 if __name__ == '__main__':
     try:
-        page_id = sys.argv[1]
+        username = sys.argv[1]
+        password = sys.argv[2]
+        keyword = sys.argv[3]
     except IndexError:
-        page_id = 'PacktPub'
-    pprint.pprint(graph(page_id))
+        print 'Usage: %s <username> <password> <keyword>' % sys.argv[0]
+    else:
+        search(username, password, keyword)
