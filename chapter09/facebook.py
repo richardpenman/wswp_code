73c0df34ecbc2cf53065346f16796e941c013fd0
Richard Penman <26063146+richardpenman@users.noreply.github.com>
2015-06-23 22:04:03 +0800

    facebook example now loads a page rather than searches

--- chapter09/facebook.py
+++ chapter09/facebook.py
@@ -4,18 +4,19 @@ import sys
 from selenium import webdriver
 
 
-def search(username, password, keyword):
+def facebook(username, password, url):
     driver = webdriver.Firefox()
     driver.get('https://www.facebook.com')
     driver.find_element_by_id('email').send_keys(username)
     driver.find_element_by_id('pass').send_keys(password)
     driver.find_element_by_id('login_form').submit()
     driver.implicitly_wait(30)
-    # search box that is available when logged in
+    # wait until the search box is available,
+    # which means have succrssfully logged in
     search = driver.find_element_by_id('q')
-    search.send_keys(keyword)
-    search.submit()
-    # Add code to scrape data of interest from Facebook search here
+    # now are logged in so can navigate to the page of interest
+    driver.get(url)
+    # add code to scrape data of interest here
     #driver.close()
  
     
@@ -23,8 +24,8 @@ if __name__ == '__main__':
     try:
         username = sys.argv[1]
         password = sys.argv[2]
-        keyword = sys.argv[3]
+        url = sys.argv[3]
     except IndexError:
-        print 'Usage: %s <username> <password> <keyword>' % sys.argv[0]
+        print 'Usage: %s <username> <password> <url>' % sys.argv[0]
     else:
-        search(username, password, keyword)
+        facebook(username, password, url)
